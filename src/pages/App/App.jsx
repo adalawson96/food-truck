9dab3d5278e12f748a7a4c8c48aa51571224b4b9
adalawson <adalawson96@gmail.com>
2023-11-01 23:04:54 +0100

    add navbar footer components and hompage folder

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,29 @@
 import './App.css';
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+import HomePage from '../HomePage/HomePage';
+// import EventsPage from "../EventsPage/EventsPage";
+// import RestaurantPage from "../RestaurantPage/RestaurantPage";
+import Footer from "../../components/Footer/Footer";
 
 export default function App() {
+  const [user, setUser] = useState();
+  
   return (
     <main className="App">
-      App
+    { user ?
+        <>
+          <NavBar user={user} setUser={setUser} />
+            <Routes>
+                <Route path="/home" element={<HomePage />} />
+                {/* <Route path="/events" element={<EventsPage />} /> */}
+                {/* <Route path="/restaurant" element={<RestaurantPage />} /> */}
+            </Routes> 
+        <Footer />
+        </>
+        : null}
     </main>
   );
 }
